02b20dd95ed232202db6bc8b5f3ee466f7dbfb07
Franklin <fabonero@ssct.edu.ph>
2025-06-18 23:17:46 +0800

    refactor database url

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,6 +1,16 @@
 import { configDotenv } from 'dotenv';
 import { Pool } from 'pg';
+import path from "path";
 configDotenv();
+
+
+configDotenv({
+    path: process.env.NODE_ENV === "production" ? path.resolve(__dirname, "../.env.production") : path.resolve(__dirname, "../.env")
+});
+
+if (process.env.NODE_ENV === "production" && process.env.DATABASE_URL_PROD) {
+    process.env.DATABASE_URL = process.env.DATABASE_URL_PROD;
+}
 
 export const isProd = process.env.NODE_ENV === "production";
 
