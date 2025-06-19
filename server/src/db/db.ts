561034f0756c0808fc7925c5766f5071bca3da92
Franklin <fabonero@ssct.edu.ph>
2025-06-19 15:47:32 +0800

    added ssl

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -9,6 +9,9 @@ configDotenv({
 export const isProd = process.env.NODE_ENV === "production";
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
