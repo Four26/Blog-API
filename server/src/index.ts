7981cfd2e7c49778fa1dadfd93ba7f655481018c
Franklin <fabonero@ssct.edu.ph>
2025-06-28 19:23:45 +0800

    refactor CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,10 +15,17 @@ const allowedOrigins = process.env.CLIENT_URL;
 
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins?.includes(origin)) return callback(null, true);
+
+        return callback(new Error("Not allowed by CORS"));
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
-    exposedHeaders: ["Content-Type", "Authorization"]
+    exposedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    optionsSuccessStatus: 200
 }));
 
 app.use(express.json());
